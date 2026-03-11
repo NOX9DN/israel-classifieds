aea73861cc71b22d26801c54905b7ed808765d97
NOX9ND <israel.project.dn@gmail.com>
2026-03-11 22:26:58 +0200

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
 };
 
 export default nextConfig;
+```
+
+Сохрани **(Ctrl+S)** потом в PowerShell:
+```
+git add .
+git commit -m "fix next config"
+git push
